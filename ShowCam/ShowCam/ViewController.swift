54ca4ecc10ce2f0b91a344974d0d01cfa3478875
lwiosbystep <1286594907@qq.com>
2016-08-19 18:46:06 +0800

    鹅鹅饭

--- ShowCam/ShowCam/ViewController.swift
+++ ShowCam/ShowCam/ViewController.swift
@@ -6,12 +6,14 @@
 //  Copyright © 2016年 lotawei. All rights reserved.
 //
 
+//还不想
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
 
